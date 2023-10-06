7b43db435a09c439d28a99bd956a4ad858d30ad9
odantonio <obdantonio@terra.com.br>
2023-10-06 11:06:59 -0300

    add: new file using static method e recursive func

--- lib/classes/utils/input.dart
+++ lib/classes/utils/input.dart
@@ -1,24 +1,37 @@
 import 'dart:convert';
 import 'dart:io';
 
-class ConsoleUtils {
-  /* 
-  Método Estático: não é necessário instanciar, podemos acessar diretamente.
-     
-  Implementando recuso Try-Catch para tratar possiveis erros, ao invés de usar .tryParse.
-  Também implementando funções recursivas.   
-     */
+/* 
+    Método Estático: não é necessário instanciar, podemos acessar diretamente.
+    
+    Implementando recuso Try-Catch para tratar possiveis erros, ao invés de usar .tryParse.
+    Também implementando funções recursivas.   
+  */
 
+class ConsoleUtils {
+/* 
+  A função abaixo pode ser usada para ler o input do console. A função apenas verifica por null.
+  Ela não recebe paramêtro: 
+*/
   static String inputText() {
     var line = stdin.readLineSync(encoding: utf8);
     return line ?? "";
   }
 
+/* 
+  A função abaixo pode ser usada para ler o input do console. A função imprime na tela um texto e verifica por null. 
+  Ela recebe apenas um paramêtro: 
+    --> message é o parâmetro da mensagem que deve estar no print
+*/
   static String inputWithText(String message) {
     print(message);
     return inputText();
   }
 
+/* 
+  A função abaixo pode ser usada para ler o input do console e fazer a conversão do tipo string para double. A função apenas verifica por null.
+  Ela não recebe paramêtro: 
+*/
   static double? inputNumber() {
     var value = inputText();
     try {
@@ -28,19 +41,32 @@ class ConsoleUtils {
     }
   }
 
+/* 
+  A função abaixo pode ser usada para ler o input do console e fazer a conversão do tipo string para double. A função imprime na tela um texto e verifica por null. 
+  Ela recebe apenas um paramêtro: 
+    --> message é o parâmetro da mensagem que deve estar no print
+*/
+
   static double? inputNumberWithText(String message) {
     print(message);
     return inputNumber();
   }
 
+/* 
+  A função abaixo pode ser usada para ler o input do console e fazer a verificação de valores não desejados. Neste caso, a função deve ser usada para converter strings para valores numéricos, e descarta entradas indesejados que não são numericas. 
+  Ela recebe dois paramêtros: 
+    --> message é o parâmetro da mensagem que deve estar no print
+    --> validation é a string que encerra a execução do código, definida em código ao chamar esta função. 
+*/
+
   static double? inputWithNumberValidation(String message, String validation) {
-    var line = inputWithText(message);
-    if (line == validation) {
-      return null;
-    }
     do {
       try {
-        return inputNumber();
+        var line = inputWithText(message);
+        if (line == validation) {
+          return null;
+        }
+        return double.parse(line);
       } catch (e) {
         print(message);
       }
